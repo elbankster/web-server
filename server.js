1c2b29427283d0e77ebbbf396acf89d2873fda16
= <=>
2016-01-22 06:23:30 -0500

    Refactor middleware

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 var express = require('express');
+var middleware = require('./middleware');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-    requireAuthentication: function(req, res, next) {
-        console.log(req.url + ' - private route hit!');
-        next();
-    },
-    logger: function(req, res, next) {
-
-        console.log(new Date().toString() + ' Request: ' + req.method + ' ' + req.originalUrl);
-        next();
-    }
-};
-
 // application-level middleware
-// app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
 
 // route-level middleware, 2nd argument in route
 app.get('/about', middleware.requireAuthentication, function(req, res) {
+    res.send('About Us!');
+});
+
+app.get('/about', function(req, res) {
     res.send('About Us!');
 });
 
